1b315083444730a6f6f11ad942c25c9dbfe5b00f
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-04 19:01:34 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
 // your Bomb code here!
+
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+
+  constructor(props) {
+    super(props)
+    // ...define initial state with a key of 'color' set to the 'value' prop
+    this.state = { color: this.props.value }
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: '#333'
+    })
+  }
+
+  render(){
+    return <div className="cell" style={{backgroundColor: this.state.color}}  onClick={this.handleClick} > </div>
+  }
+
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,15 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { currentSlideIndex: 0 }
+  }
+
+  render(){
+    return <div>I am on slide ${this.state.currentSlideIndex} </div>
+  }
+
+}
