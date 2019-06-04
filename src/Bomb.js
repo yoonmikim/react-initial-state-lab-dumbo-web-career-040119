ac7ebfc312d935bd1770bc3ab0855918c9e94899
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-04 19:09:26 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
 // your Bomb code here!
+
+import React from 'react';
+
+export default class Cell extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { secondsLeft: this.props.initialCount }
+  }
+
+  render() {
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>{message}</div>
+    )
+  }
+}
